f976344cd7ff4255169731ca58a92d69e86a4a19
profdouglasbarcelos <profdouglasbarcelos@gmail.com>
2017-04-07 22:56:23 -0300

    Criacao do projeto com EntityFramework

---
+++ InstituicaoEnsino/Models/Aluno.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Aluno
+    {
+        public int AlunoID { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+    }
+}
---
+++ InstituicaoEnsino/Models/DAL/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto() : base("strConn")
+        {
+
+        }
+        
+        public DbSet<Aluno> Alunos { get; set; }
+        public DbSet<Curso> Cursos { get; set; }
+    }
+}
