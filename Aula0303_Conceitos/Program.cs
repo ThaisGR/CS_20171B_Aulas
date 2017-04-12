68f52848f2c8482d883751fe23efd049840df43c
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2017-04-12 19:26:27 -0300

    Corrigindo bugs

--- Aula0303_Conceitos/Program.cs
+++ Aula0303_Conceitos/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using static Aula0303_Conceitos.Util;
+using Aula0303_Conceitos;
 
 namespace Aula0303_Conceitos
 {
@@ -12,11 +12,11 @@ namespace Aula0303_Conceitos
             MeioTransporte mt = new Barco();
             ((Barco)mt).QuantidadePes = 10;
 
-            TipoMeioTransporte tipo = (TipoMeioTransporte) Menu();
+            Aula0303_Conceitos.Util.TipoMeioTransporte tipo = (Aula0303_Conceitos.Util.TipoMeioTransporte) Util.Menu();
 
             switch (tipo)
             {
-                case TipoMeioTransporte.Carro:
+                case Aula0303_Conceitos.Util.TipoMeioTransporte.Carro:
                     Carro c = new Carro("Fusca");
                     c.Ano = 1978;
                     c.Capacidade = 4;
@@ -24,7 +24,7 @@ namespace Aula0303_Conceitos
                     c.Frear();
                     c.Parar();
                     break;
-                case TipoMeioTransporte.Bicicleta:
+                case Aula0303_Conceitos.Util.TipoMeioTransporte.Bicicleta:
                     Bicicleta b = new Bicicleta();
                     b.Nome = "Caloi";
                     b.Ano = 2016;
@@ -32,7 +32,7 @@ namespace Aula0303_Conceitos
                     b.QuantidadeMarcha = 21;
                     b.Parar();
                     break;
-                case TipoMeioTransporte.Barco:
+                case Aula0303_Conceitos.Util.TipoMeioTransporte.Barco:
                     Barco ba = new Barco();
                     ba.Ano = 1969;
                     ba.Nome = "Titanic";
